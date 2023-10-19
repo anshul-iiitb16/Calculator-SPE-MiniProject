141aa915a2a099c1d57249bbd127fd8654db972e
anshul-iiitb16 <Anshul.Jindal@iiitb.org>
2023-10-20 00:47:39 +0530

    Implemented Multiplication

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,4 +1,4 @@
-package org.example;
+// package org.example;
 
 import java.util.*;
 
@@ -10,7 +10,13 @@ public class Calculator
         double res = n1 + n2;
         return res;
     }
-    
+
+    public static double Multiply(double n1, double n2)
+    {
+        double res = n1*n2;
+        return res;
+    }
+
     
     public static void main(String[] args) 
     {
@@ -37,6 +43,21 @@ public class Calculator
             double result = Add(num1, num2);
 
             System.out.println("The Final Result is : " + result);
+        }
+        else if(op == 2)
+        {
+            System.out.print("Please Enter 2 numbers to be Multiplied: ");
+
+            double num1 = sc.nextDouble();
+            double num2 = sc.nextDouble();
+
+            double result = Multiply(num1, num2);
+
+            System.out.println("The Final Result is : " + result);
+        }
+        else
+        {
+            System.out.println("Thanks for Using Calculator Program - BYE");
         }
 
         sc.close();
