7471f89d47cf59e81b09fadc771359ebe0a8ec5f
anshul-iiitb16 <Anshul.Jindal@iiitb.org>
2023-10-20 00:30:27 +0530

    Implemented Addition

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.util.*;
+
+public class Calculator 
+{
+
+    public static double Add(double n1, double n2)
+    {
+        double res = n1 + n2;
+        return res;
+    }
+    
+    
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("-------- Welcome to Calculator Program - SPE ---------");
+        System.out.println("");
+        System.out.println("-------- Please Enter the Operation Code: ------------");
+        System.out.println("1. Addition");
+        System.out.println("2. Multiplication");
+        System.out.println("3. Exit");
+
+        System.out.println("");
+
+        int op = sc.nextInt();
+
+        if(op == 1)
+        {
+            System.out.print("Please Enter 2 numbers to be added: ");
+
+            double num1 = sc.nextDouble();
+            double num2 = sc.nextDouble();
+
+            double result = Add(num1, num2);
+
+            System.out.println("The Final Result is : " + result);
+        }
+
+        sc.close();
+
+    }
+}
