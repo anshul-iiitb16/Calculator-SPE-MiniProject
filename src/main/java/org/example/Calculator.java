ac5c1508bc2182f673e96be3a4643606814c801d
anshul-iiitb16 <Anshul.Jindal@iiitb.org>
2023-10-28 17:17:09 +0530

    Added Sqrt and factorial functions

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -17,6 +17,28 @@ public class Calculator
         return res;
     }
 
+    public static int Factorial(int n1)
+    {
+        if(n1 < 0)
+            return -1;
+
+        int res=1;
+
+        for(int i=1;i<=n1;i++)
+            res*=i;
+
+        return res;
+    }
+
+    public static double Square_Root(double n1)
+    {
+        if(n1 < 0)
+            return -1;
+
+        double res = Math.sqrt(n1);
+        return res;
+    }
+
     
     public static void main(String[] args) 
     {
@@ -27,7 +49,9 @@ public class Calculator
         System.out.println("-------- Please Enter the Operation Code: ------------");
         System.out.println("1. Addition");
         System.out.println("2. Multiplication");
-        System.out.println("3. Exit");
+        System.out.println("3. Factorial");
+        System.out.println("4. Square_Root");
+        System.out.println("5. Exit");
 
         System.out.println("");
 
@@ -54,6 +78,32 @@ public class Calculator
             double result = Multiply(num1, num2);
 
             System.out.println("The Final Result is : " + result);
+        }
+        else if(op == 3)
+        {
+            System.out.print("Please Enter integer whose factorial you want to calculate: ");
+
+            int num1 = sc.nextInt();
+
+            int result = Factorial(num1);
+
+            if(result == -1)
+                System.out.println("Please Enter valid non-negative Integer Input");
+            else
+                System.out.println("The Final Result is : " + result);
+        }
+        else if(op == 4)
+        {
+            System.out.print("Please Enter number whose Sqrt you want to calculate: ");
+
+            double num1 = sc.nextDouble();
+
+            double result = Square_Root(num1);
+
+            if(result == -1)
+                System.out.println("Please Enter valid Input >= 0");
+            else
+                System.out.println("The Final Result is : " + result);
         }
         else
         {
